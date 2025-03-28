d5467ffe608cd2a7f03eee211da72033e7440d31
BMLam <mailto@bmlam.de>
2025-03-28 22:19:58 +0100

    created imp table for Consors Tagesgeld. to be tested

---
+++ supporting_objects/lam/tables/imp_consors_csv_deutsch.sql
@@ -0,0 +1,14 @@
+CREATE TABLE  imp_consors_csv_deutsch 
+   (	"Buchung" DATE, 
+	"Valuta" DATE, 
+	"Sender / Empfänger" VARCHAR2(100 CHAR), 
+	"IBAN" VARCHAR2(20 CHAR), 
+	"BIC" VARCHAR2(20 CHAR), 
+	"Buchungstext" VARCHAR2(200 CHAR), 
+	"Verwendungszweck" VARCHAR2(100 CHAR), 
+	"Betrag" VARCHAR2(10 CHAR), 
+	"Währung" VARCHAR2(3 CHAR), 
+	"INSERT_DT" DATE DEFAULT sysdate, 
+	"APEX_SESS_ID" NUMBER
+   )
+/
